0c864a2213920b718dd8d11fe5cf8b15c30367d4
Joshil S Abraham <80256698+mighty070jail@users.noreply.github.com>
2025-02-05 16:10:59 +0530

    Update sum.sh

--- shell/sum.sh
+++ shell/sum.sh
@@ -1,3 +1,9 @@
+<<com 
+      Name: Joshil S Abraham
+      Date: 05/02/2025
+      Description: Script to find the Sum of Two numbers.
+com
+
 read -p "Enter The First Number: " num1
 read -p "Enter The Second Number: " num2
 result=$((num1 + num2))
