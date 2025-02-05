b19a58ead7eac315f81952d21f15420ccf485094
Joshil S Abraham <80256698+mighty070jail@users.noreply.github.com>
2025-02-05 16:08:36 +0530

    Update largest.sh

--- shell/largest.sh
+++ shell/largest.sh
@@ -1,3 +1,9 @@
+<<com 
+      Name: Joshil S Abraham
+      Date: 05/02/2025
+      Description: Script to display largest of two numbers.
+com
+
 read -p "Enter The First Number: " num1
 read -p "Enter The Second Number: " num2
 if [ $num1 -gt $num2 ]
@@ -6,4 +12,3 @@ echo "$num1 is greater than $num2"
 else
 echo "$num2 is greater than $num1"
 fi
-
