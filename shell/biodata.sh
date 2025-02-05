2a7e7499313976173f3f650d4e9343772f18f35c
Joshil S Abraham <80256698+mighty070jail@users.noreply.github.com>
2025-02-05 16:07:31 +0530

    Update biodata.sh

--- shell/biodata.sh
+++ shell/biodata.sh
@@ -1,7 +1,12 @@
+<<com 
+      Name: Joshil S Abraham
+      Date: 05/02/2025
+      Description: Script that collects user information through prompts.
+com
+
 read -p "Enter Name: " name
 read -p "Date of Birth: " dob
 read -p "Gender: " gender
 read -p "Nationality: " nationality
 read -p "Marital Status: " marital_status
 read -p "Languages Known: " languages
-
