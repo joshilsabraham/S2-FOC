ab8059e209ec180ed79235f8dfbf6baeb4e0dbfe
Joshil S Abraham <80256698+mighty070jail@users.noreply.github.com>
2025-02-17 11:51:48 +0530

    Update factorial.sh

--- shell/factorial.sh
+++ shell/factorial.sh
@@ -1,14 +1,17 @@
-<<com 
-      Name: Joshil S Abraham
-      Date: 05/02/2025
-      Description: Script that calculates the Factorial of a given number. 
-com
+# <<com 
+#       Name: Joshil S Abraham
+#       Date: 05/02/2025
+#       Description: Script that calculates the Factorial of a given number. 
+# com
 
 read -p "Enter a Number: " number
 factorial=1
-while [$number -gt 1]
-do         
-	factorial = $((factorial*number))
-	number=$((number-1))
-done    
-echo "The Factorial of $number is $factorial"
+original_number=$number
+
+while [ $number -gt 1 ]
+do
+    factorial=$((factorial * number))
+    number=$((number - 1))
+done
+
+echo "The Factorial of $original_number is $factorial"
