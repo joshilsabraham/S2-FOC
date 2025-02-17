a9cb42b1813736ee87712de7f554fd0b7e8ba38a
Joshil S Abraham <80256698+mighty070jail@users.noreply.github.com>
2025-02-17 11:34:04 +0530

    Create numbers.sh

---
+++ shell/numbers.sh
@@ -0,0 +1,11 @@
+<<com 
+      Name: Joshil S Abraham
+      Date: 17/02/2025
+      Description: Script that prints number upto the given Limit using for loop.
+com
+
+read -p "Enter The Limit: " limit
+for((i=0;i<=limit;i++))
+do
+echo $i
+done
